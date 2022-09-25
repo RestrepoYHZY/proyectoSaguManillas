26d8c095b8a2136899d5358f24a2077aa61f506e
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-09-25 13:49:30 -0500

    Estilos Mejorados

--- indexTodo.php
+++ indexTodo.php
@@ -6,49 +6,59 @@ include("./admin/config/db.php");
 
 <div class="main-section">
     <div class="add-section">
-        <div class="container col-12 mb-1">
+        <div class="text-center fs-3 mb-3 font">
             <h1>Lista de Pedidos</h1>
         </div>
-        <form action="todolist/addTodo.php" method="POST" autocomplete="off">
-            <?php if (isset($_GET['mess']) && $_GET['mess'] == 'error') { ?>
-                <input type="text" name="titulo" style="border-color: #ff6666" placeholder="Campo Obligatorio" />
-                <button type="submit">Add &nbsp; <span>&#43;</span></button>
-            <?php } else { ?>
-                <input type="text" name="titulo" placeholder="Anota un pedido :)" />
-                <button type="submit">Add &nbsp; <span>&#43;</span></button>
-            <?php } ?>
-        </form>
+        <div class="card mb-2">
+            <div class="card-body">
+                <form action="todolist/addTodo.php" method="POST" autocomplete="off">
+                    <?php if (isset($_GET['mess']) && $_GET['mess'] == 'error') { ?>
+                        <input type="text" name="titulo" style="border-color: #ff6666" placeholder="No olvides escribir tu pedido!" maxlength="500" />
+                        <button type="submit" class="btn btn-primary">Añadir <i class='bx bx-message-square-add'></i> </button>
+                    <?php } else { ?>
+                        <input type="text" name="titulo" placeholder="Anota un pedido :)" maxlength="500" />
+                        <button type="submit" class="btn btn-primary">Añadir <i class='bx bx-message-square-add'></i> </button>
+                    <?php } ?>
+                </form>
+            </div>
+        </div>
+
     </div>
     <?php
     $todos = $conexion->query("SELECT * FROM todo_list ORDER BY idTodo DESC");
     ?>
-    <div class="show-todo-section">
-        <?php if ($todos->rowCount() <= 0) { ?>
-            <div class="todo-item">
-                <div class="empty">
-                    <img src="./image/img1.png" width="100%">
-                    <img src="./image/Ellipsis.gif" width="80px">
-                </div>
-            </div>
-        <?php } ?>
-
-        <?php while ($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
-            <div class="todo-item">
-                <span idTodo="<?php echo $todo['idTodo']; ?>" class="remove-to-do">x</span>
-                <?php if ($todo['checked']) { ?>
-                    <input type="checkbox" data-todo-id=" <?php echo $todo['idTodo']; ?>" class="check-box" checked />
-                    <h2 class="checked"><?php echo $todo['titulo'] ?></h2>
-
-                <?php } else { ?>
-                    <input type="checkbox" class="check-box" data-todo-id="<?php echo $todo['idTodo']; ?>" />
-                    <h2><?php echo $todo['titulo'] ?></h2>
+    <div class="card">
+        <div class="card-body">
+            <div class="show-todo-section">
+                <?php if ($todos->rowCount() <= 0) { ?>
+                    <div class="todo-item">
+                        <div class="empty">
+                            <img src="./image/img1.png" width="100%">
+                            <img src="./image/Ellipsis.gif" width="80px">
+                        </div>
+                    </div>
                 <?php } ?>
-                <br />
-                <small>Creado: <?php echo $todo['fecha'] ?> </small>
-            </div>
-        <?php } ?>
 
+                <?php while ($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+                    <div class="todo-item">
+                        <span idTodo="<?php echo $todo['idTodo']; ?>" class="remove-to-do"><i class='bx bx-message-square-x'></i></span>
+                        <?php if ($todo['checked']) { ?>
+                            <input type="checkbox" data-todo-id=" <?php echo $todo['idTodo']; ?>" class="check-box" checked />
+                            <h2 class="checked"><?php echo $todo['titulo'] ?></h2>
+
+                        <?php } else { ?>
+                            <input type="checkbox" class="check-box" data-todo-id="<?php echo $todo['idTodo']; ?>" />
+                            <h2><?php echo $todo['titulo'] ?></h2>
+                        <?php } ?>
+                        <br />
+                        <small>Creado: <?php echo $todo['fecha'] ?> </small>
+                    </div>
+                <?php } ?>
+
+            </div>
+        </div>
     </div>
+
 </div>
 
 <script src="js/jquery-3.6.0.min.js"></script>
